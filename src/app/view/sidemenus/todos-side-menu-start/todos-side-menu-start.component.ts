c701ec9639d5934e44d78cd9dbee83f8f107a056
Florian Schwanz <florian.schwanz@interoberlin.de>
2018-04-19 08:36:33 +0200

    Fixed tags in todo page

--- src/app/view/sidemenus/todos-side-menu-start/todos-side-menu-start.component.ts
+++ src/app/view/sidemenus/todos-side-menu-start/todos-side-menu-start.component.ts
@@ -1,4 +1,5 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Tag} from '../../../model/tag.model';
 
 @Component({
   selector: 'app-todos-side-menu-start',
@@ -6,15 +7,18 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
   styleUrls: ['./todos-side-menu-start.component.scss']
 })
 export class TodosSideMenuStartComponent implements OnInit {
-  @Input() tags = [];
+  @Input() tags = new Map<string, Tag>();
   @Input() priorities = [];
   @Output() onTagChangedEmitter = new EventEmitter<string>();
   @Output() onPrioritySelectedEmitter = new EventEmitter<string>();
+
+  tagList = [];
 
   constructor() {
   }
 
   ngOnInit() {
+    this.tagList = Array.from(this.tags.values());
   }
 
   onTagChanged() {
