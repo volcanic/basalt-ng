41c38578f048ead6c536dbec444b2bf83655c2f6
Florian Schwanz <florian.schwanz@interoberlin.de>
2018-06-26 01:39:04 +0200

    Displayed tasks in sidenav

--- src/app/view/components/lists/task-list/task-list.component.ts
+++ src/app/view/components/lists/task-list/task-list.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {TaskService} from '../../../../services/entities/task.service';
+import {Subject} from 'rxjs/Rx';
+import {takeUntil} from 'rxjs/internal/operators';
+import {DateService} from '../../../../services/date.service';
 import {Task} from '../../../../model/entities/task.model';
 
 @Component({
@@ -9,17 +13,26 @@ import {Task} from '../../../../model/entities/task.model';
 export class TaskListComponent implements OnInit {
 
   tasksOverdue = [];
-  tasksToday = [];
-  tasksLater = [];
+  tasksNext = [];
+  tasksInbox = [];
 
-  constructor() {
+  private tasksUnsubscribeSubject = new Subject();
+
+  constructor(private taskService: TaskService,
+              private dateService: DateService) {
   }
 
   ngOnInit() {
 
-    let t = new Task('Hello 1', "world");
-    this.tasksOverdue.push(t);
+    // Subscribe tasklet changes
+    this.taskService.tasksSubject.pipe(
+      takeUntil(this.tasksUnsubscribeSubject)
+    ).subscribe((value) => {
+      if (value != null) {
+        this.tasksOverdue = (value as Task[]).filter(task => task != null && task.dueDate != null && this.dateService.isBefore(task.dueDate, new Date()));
+        this.tasksNext = (value as Task[]).filter(task => task != null && task.dueDate != null && this.dateService.isAfter(task.dueDate, new Date()));
+        this.tasksInbox = (value as Task[]).filter(task => task == null || task.dueDate == null);
+      }
+    });
   }
-
-
 }
