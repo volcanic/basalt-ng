54ddc6fc7093b8d8180b13d6aaa6b5a4e65b9208
Florian Schwanz <florian.schwanz@interoberlin.de>
2018-06-27 00:08:31 +0200

    Make due date settable

--- src/app/view/dialogs/other/task-dialog/task-dialog.component.ts
+++ src/app/view/dialogs/other/task-dialog/task-dialog.component.ts
@@ -1,8 +1,8 @@
 import {Component, Inject, OnInit} from '@angular/core';
-import {FormControl} from '@angular/forms';
 import {Task} from '../../../../model/entities/task.model';
-import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+import {DateAdapter, MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
 import {DIALOG_MODE} from '../../../../model/dialog-mode.enum';
+import {DateService} from '../../../../services/date.service';
 
 @Component({
   selector: 'app-task-dialog',
@@ -17,14 +17,37 @@ export class TaskDialogComponent implements OnInit {
   dialogTitle = '';
   task: Task;
 
-  constructor(public dialogRef: MatDialogRef<TaskDialogComponent>,
+  hour = 0;
+  minute = 0;
+  hours = [];
+  minutes = [];
+
+  constructor(private adapter: DateAdapter<any>,
+              public dialogRef: MatDialogRef<TaskDialogComponent>,
               @Inject(MAT_DIALOG_DATA) public data: any) {
   }
 
   ngOnInit() {
+    this.adapter.setLocale('en-GB');
+
     this.mode = this.data.mode;
     this.dialogTitle = this.data.dialogTitle;
     this.task = JSON.parse(this.data.task);
+    if (this.task.dueDate == null) {
+      this.task.dueDate = new Date();
+    }
+    this.hour = this.task.dueDate.getHours();
+    this.minute = this.task.dueDate.getMinutes();
+
+    for (let h = 0; h < 24; h++) {
+      this.hours.push(h);
+    }
+    for (let m = 0; m < 60; m = m + DateService.MINUTES_INTERVAL) {
+      this.minutes.push(m);
+    }
+
+    this.hour = new Date(this.task.dueDate).getHours();
+    this.minute = new Date(this.task.dueDate).getMinutes();
   }
 
   addTask() {
@@ -33,5 +56,21 @@ export class TaskDialogComponent implements OnInit {
 
   updateTask() {
     this.dialogRef.close(this.task);
+  }
+
+  onHourSelected(value: number) {
+    this.task.dueDate = new Date(this.task.dueDate.getFullYear(), this.task.dueDate.getMonth(), this.task.dueDate.getDate(), value, this.task.dueDate.getMinutes());
+  }
+
+  onMinuteSelected(value: number) {
+    this.task.dueDate = new Date(this.task.dueDate.getFullYear(), this.task.dueDate.getMonth(), this.task.dueDate.getDate(), this.task.dueDate.getHours(), value);
+  }
+
+  addTrailingZero(value: number) {
+    if (value < 10) {
+      return `0${value}`;
+    }
+
+    return value;
   }
 }
