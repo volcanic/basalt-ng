70ae01abc16363503cb8d54e786ce12666bfb2c6
Matteo Ramin <matt.ramin@gmail.com>
2018-06-28 22:37:50 +0200

    Fixed tag behaviour on removal

--- src/app/view/dialogs/tasklet/tasklet-dialog-tags/tasklet-dialog-tags.component.ts
+++ src/app/view/dialogs/tasklet/tasklet-dialog-tags/tasklet-dialog-tags.component.ts
@@ -18,20 +18,38 @@ export class TaskletDialogTagsComponent implements OnInit {
   ngOnInit() {
   }
 
+  /**
+   * is being called whenever there are changed made to the list of tags in a tasklet
+   */
   tagChanged() {
-    let noEmptyTag = true;
+    let hasEmptyTag = false; // holds information about whether there is an empty tag in the list of tags or not
 
-    this.newTags.forEach((t: Tag) => {
-        if (t.name.trim().length === 0) {
-          noEmptyTag = false;
-          t.checked = false;
-        } else {
-          t.checked = true;
+    this.newTags.forEach((t: Tag, loopIndex: number) => {
+      if (t.name.trim().length === 0) { // if the tag is empty
+        hasEmptyTag = true; // set the tag
+        t.checked = false; // remove the tick from the checkbox
+
+        // If the current empty tag is at second last position, pop the last tag
+        if ( loopIndex === this.newTags.length - 2 ) {
+
+         this.newTags.pop();
+
+        } else if ( loopIndex < this.newTags.length - 2) {
+
+          // if the current empty tag is before second last position, splice array
+          this.newTags.splice(loopIndex, 1);
+          // TODO: Switch cursor focus to the empty tag after splicing
+
+         } else {
+          // If the current empty tag is at the end of the list, do nothing
         }
-      }
-    );
 
-    if (noEmptyTag) {
+      } else {
+        t.checked = true; // if the tag has content, i.e. user starts typing, tick the checkbox
+      }
+    });
+
+    if (!hasEmptyTag) {
       this.newTags.push(new Tag('', false));
     }
   }
