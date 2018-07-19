252bb6e8bf2649522fb879a6938342aadca98990
Florian Schwanz <florian.schwanz@interoberlin.de>
2018-07-19 19:11:44 +0200

    Improved splash screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,7 +39,7 @@ export class AppComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    // this.initializeDatabaseSync();
+    this.initializeDatabaseSync();
   }
 
   initializeDatabaseSync() {
