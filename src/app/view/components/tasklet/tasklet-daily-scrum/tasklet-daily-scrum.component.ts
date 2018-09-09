ef8cbaaa3c32034e04723266c8513d326aad24f9
Florian Schwanz <florian.schwanz@interoberlin.de>
2018-09-09 13:43:10 +0200

    Turned tasklet daily scrum into a dumb component

--- src/app/view/components/tasklet/tasklet-daily-scrum/tasklet-daily-scrum.component.ts
+++ src/app/view/components/tasklet/tasklet-daily-scrum/tasklet-daily-scrum.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
 import {TaskletDailyScrum} from '../../../../model/entities/scrum/tasklet-daily-scrum.model';
 import {DailyScrumActivityType} from '../../../../model/entities/scrum/daily-scrum-activity-type.enum';
 
@@ -8,13 +8,14 @@ import {DailyScrumActivityType} from '../../../../model/entities/scrum/daily-scr
 @Component({
   selector: 'app-tasklet-daily-scrum',
   templateUrl: './tasklet-daily-scrum.component.html',
-  styleUrls: ['./tasklet-daily-scrum.component.scss']
+  styleUrls: ['./tasklet-daily-scrum.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TaskletDailyScrumComponent {
 
   /** Tasklet to be displayed */
   @Input() tasklet: TaskletDailyScrum;
 
-  /** Reference to static method */
-  type = DailyScrumActivityType;
+  /** Enum for daily scrum activity types */
+  dailyScrumActivityType = DailyScrumActivityType;
 }
