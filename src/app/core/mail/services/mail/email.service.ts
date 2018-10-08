255320925dac060a4b6f3294d49af5de63853db7
Florian Schwanz <florian.schwanz@interoberlin.de>
2018-10-08 07:51:33 +0200

    Fixed mail sending for incomplete data

--- src/app/core/mail/services/mail/email.service.ts
+++ src/app/core/mail/services/mail/email.service.ts
@@ -16,10 +16,10 @@ export class EmailService {
    * @param text text
    */
   sendMail(recipients: string[], carbonCopy: string[], subject: string, text: string) {
-    window.location.href = `mailto:${recipients.join(';')}`
-      + `?cc=${carbonCopy.join(',')}`
-      + `&subject=${escape(subject)}`
-      + `&body=${escape(text)}`
-    ;
+    window.location.href = `mailto:`
+      + (recipients != null && recipients.length > 0 ? `${recipients.join(',')}` : '')
+      + (carbonCopy != null && carbonCopy.length > 0 ? `?cc=${carbonCopy.join(',')}` : '')
+      + (subject != null && subject.length > 0 ? `&subject=${escape(subject)}` : '')
+      + (text != null && text.length > 0 ? `&body=${escape(text)}` : '');
   }
 }
