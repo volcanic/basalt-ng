31501b0c6b6f747f6632280d626d770e5480b0d4
Florian Schwanz <florian.schwanz@interoberlin.de>
2018-10-21 00:46:02 +0200

    Added name pipes

---
+++ src/app/pages/timeline/pipes/person-name.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { PersonNamePipe } from './person-names.pipe';
+
+describe('PersonNamePipe', () => {
+  it('create an instance', () => {
+    const pipe = new PersonNamePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ src/app/pages/timeline/pipes/person-names.pipe.ts
@@ -0,0 +1,19 @@
+import {Pipe, PipeTransform} from '@angular/core';
+import {Person} from '../../../core/entity/model/person.model';
+
+@Pipe({
+  name: 'personNames'
+})
+export class PersonNamesPipe implements PipeTransform {
+
+  /**
+   * Transforms a list of persons into a list of their names
+   * @param persons lists of persons
+   * @param args
+   */
+  transform(persons: Person[], args?: any): string[] {
+    return persons.map(person => {
+      return person.name;
+    });
+  }
+}
