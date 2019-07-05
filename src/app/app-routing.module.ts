9881d4db94daca02726ab1dcccf937cd2a54211a
Florian Schwanz <florian.schwanz@interoberlin.de>
2019-07-05 15:44:10 +0200

    Disabled tracing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,7 +21,7 @@ const routes: Routes = [
 ;
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {enableTracing: true})],
+  imports: [RouterModule.forRoot(routes, {enableTracing: false})],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
