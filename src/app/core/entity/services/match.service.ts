d1de8e653259233269d5aa90432b12fcc96c60b3
Florian Schwanz <florian.schwanz@interoberlin.de>
2019-07-25 08:25:44 +0200

    Checked for null values

--- src/app/core/entity/services/match.service.ts
+++ src/app/core/entity/services/match.service.ts
@@ -467,9 +467,11 @@ export class MatchService {
    * @returns true if description matches search item
    */
   private descriptionMatchesSingleItem(description: Description, item: string): boolean {
-    return description.value != null && description.value.split('\n').some(s => {
-      return MatchService.textMatchesSingleItem(s, item);
-    });
+    return description != null
+      && description.value != null
+      && description.value.split('\n').some(s => {
+        return MatchService.textMatchesSingleItem(s, item);
+      });
   }
 
   /**
